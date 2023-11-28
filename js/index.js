43048d71c371ff83f0a762fccef5d1f42feb8d39
Alejo Fernandez <fernandezalejo981@gmail.com>
2023-11-28 18:11:23 -0300

    responsive del index, sass implementado

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.explicacion-text, .explicacion-text-nosotros', {
+        duration: 2000,
+        origin: 'left',
+        distance: '400px'
+    });
+    sr.reveal('.explicacion-text-nosotros', {
+        duration: 2000,
+        origin: 'right',
+        distance: '400px'
+    });
